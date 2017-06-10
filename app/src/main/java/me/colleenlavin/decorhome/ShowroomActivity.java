6a20bc17a8e09f33d327bdc442ce0d1bf9ca1bf4
mastrgamr <themastrgamr@gmail.com>
2017-06-10 18:46:28 -0400

    Added Showroom Activity, Colorscheme Chooser

---
+++ app/src/main/java/me/colleenlavin/decorhome/ShowroomActivity.java
@@ -0,0 +1,62 @@
+package me.colleenlavin.decorhome;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+import com.mikepenz.fastadapter.FastAdapter;
+import com.mikepenz.fastadapter.IAdapter;
+import com.mikepenz.fastadapter.commons.adapters.FastItemAdapter;
+
+import java.util.ArrayList;
+
+public class ShowroomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_showroom);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+        RecyclerView recView = (RecyclerView)findViewById(R.id.showroom_chooser_recview);
+        ArrayList<SimpleItem> items = new ArrayList<>();
+        for(int i = 0; i < 50; i++){
+            items.add(new SimpleItem().withName("Showroom #" + i));
+        }
+        //create our FastAdapter which will manage everything
+        FastItemAdapter fastAdapter = new FastItemAdapter();
+        //set the items to your ItemAdapter
+        fastAdapter.add(items);
+        fastAdapter.withOnClickListener(new FastAdapter.OnClickListener<SimpleItem>() {
+            @Override
+            public boolean onClick(View v, IAdapter<SimpleItem> adapter, SimpleItem item, int position) {
+                Toast.makeText(v.getContext(), (item).name, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+        //set our adapters to the RecyclerView
+        //we wrap our FastAdapter inside the ItemAdapter -> This allows us to chain adapters for more complex useCases
+        recView.setAdapter(fastAdapter);
+        recView.setLayoutManager(new LinearLayoutManager(this));
+        recView.setItemAnimator(new DefaultItemAnimator());
+    }
+
+}
