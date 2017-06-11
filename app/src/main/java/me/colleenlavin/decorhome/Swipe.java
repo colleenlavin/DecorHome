bf7e4efbad974324c394aac127d1cae8ab68fbd6
colleenlavin <cml388@mun.ca>
2017-06-10 20:30:48 -0400

    Adding swipe

---
+++ app/src/main/java/me/colleenlavin/decorhome/Swipe.java
@@ -0,0 +1,14 @@
+package me.colleenlavin.decorhome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.daprlabs.aaron.swipedeck.SwipeDeck;
+
+public class Swipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe);
+    }
+}
