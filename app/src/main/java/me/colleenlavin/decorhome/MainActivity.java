8484676643fcdfa94df8c21588f0d117c35faf67
mastrgamr <themastrgamr@gmail.com>
2017-06-10 20:34:57 -0400

    click the first button lol

--- app/src/main/java/me/colleenlavin/decorhome/MainActivity.java
+++ app/src/main/java/me/colleenlavin/decorhome/MainActivity.java
@@ -1,5 +1,6 @@
 package me.colleenlavin.decorhome;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +42,15 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        ImageButton livingRoomBtn = (ImageButton) findViewById(R.id.img_living_room);
+        livingRoomBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ColorSchemeChooserActvity.class));
+            }
+        });
+        ImageButton bathroom = (ImageButton) findViewById(R.id.img_bathroom);
     }
 
     @Override
